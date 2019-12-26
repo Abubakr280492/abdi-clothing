351432e1f3c26fd4939a607e60ae4e3477955b25
Abubakr280492 <you@example.com>
2019-12-26 21:06:10 +0500

    fired firebase utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -22,8 +22,27 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData)=>{
   if (!userAuth) return;
 
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-}
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    }catch (error){
+      console.log('error created user', console.error.massage );
+    }
+  }
+return userRef;
+
+};
 
 
 firebase.initializeApp(config);
