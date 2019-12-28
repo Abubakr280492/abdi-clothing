757ff6abaa6b9c56abd93a960c4c8535f87edcb4
Abubakr280492 <you@example.com>
2019-12-29 02:24:25 +0500

    remove item at checkout

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -4,7 +4,8 @@
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
     ADD_ITEM :'ADD_ITEM',   //add item action 
-    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
+    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART',
+    REMOVE_ITEM: 'REMOVE_ITEM'
 };
 
 export default CartActionTypes;
