002272b9021f0426c78e260f666b33c599d450e0
Abubakr280492 <you@example.com>
2019-12-30 17:30:37 +0500

    redux persist

--- src/index.js
+++ src/index.js
@@ -6,18 +6,22 @@ import ReactDOM from 'react-dom';
 import{BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
-import store from './redux/store';
-
+import {store,persistor} from './redux/store';
+ 
 
 import './index.css';
 import App from './App';
 
+import {PersistGate} from 'redux-persist/integration/react';
+//import {persistReducer} from 'redux-persist';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 <Provider store={store}>
    <BrowserRouter>
+    <PersistGate persistor={persistor}>
       <App />
+      </PersistGate>
    </BrowserRouter>
 </Provider>,
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,9 +7,11 @@ import {createStore, applyMiddleware }  from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
+import{persistStore} from 'redux-persist';
 const middlewares = [logger];
 
-const store = createStore(rootReducer,applyMiddleware(...middlewares));
+export const store = createStore(rootReducer,applyMiddleware(...middlewares));
 
+export const persistor = persistStore(store);
 
-export default store;
+export default {store,persistor};
